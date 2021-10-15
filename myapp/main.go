f8af57f2d6c671f77a8e253ac6c87613d0c485c6
Vachira J <cyberwizard.dev@gmail.com>
2021-10-15 23:08:10 +0700

    Starting wotk on Celeritas

--- myapp/main.go
+++ myapp/main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/cyberlogs/celeritas"
-)
-
 func main() {
-	result := celeritas.TestFunc(1, 1)
-	fmt.Println(result)
-
-	result = celeritas.TestFunc2(2, 1)
-
-	fmt.Println(result)
-
-	result = celeritas.TestFunc3(2, 2)
-	fmt.Println(result)
 
 }
